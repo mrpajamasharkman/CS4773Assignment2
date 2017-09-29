e07d531ec89186b05d0cc6ebc23117355e623c9c
WoernerBro <seanwoerner.ru89@gmail.com>
2017-09-29 15:14:22 -0500

    Imported JUnit and created empty test class

---
+++ src/test/TestWar.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Scanner;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestWar {
+	
+}
